9c4d2997d03ed13951296c468cbc8bb81f37f279
akbarsury <190411100119@student.trunojoyo.ac.id>
2023-06-07 17:15:48 +0700

    v2

--- assets/Loader-37339fd0.js
+++
@@ -1,2 +0,0 @@
-import{_ as s,o as _,c as n,f as e}from"./main-284d8624.js";const o={},c={class:"loader"},t=e("span",null,[e("i",{class:"fa-solid fa-spinner"})],-1),a=[t];function r(i,l){return _(),n("div",c,a)}const p=s(o,[["render",r]]);export{p as _};
-//# sourceMappingURL=Loader-37339fd0.js.map
