f36fdf9c877cf4c1bd4508dd192c085b8bdb0d1a
akbarsury <190411100119@student.trunojoyo.ac.id>
2023-06-07 19:59:57 +0700

    v2

---
+++ assets/Loader-bbe06be2.js
@@ -0,0 +1,2 @@
+import{_ as s,o as _,c as n,f as e}from"./main-545820b8.js";const o={},c={class:"loader"},t=e("span",null,[e("i",{class:"fa-solid fa-spinner"})],-1),a=[t];function r(i,l){return _(),n("div",c,a)}const p=s(o,[["render",r]]);export{p as _};
+//# sourceMappingURL=Loader-bbe06be2.js.map
