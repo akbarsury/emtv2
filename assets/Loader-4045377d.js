c198ce992315123b29952c431c1691fe49dbd80b
akbarsury <190411100119@student.trunojoyo.ac.id>
2023-06-07 16:47:19 +0700

    v2

--- assets/Loader-4045377d.js
+++
@@ -1,2 +0,0 @@
-import{_ as s,o as _,c as n,e}from"./main-ea7617f2.js";const o={},c={class:"loader"},t=e("span",null,[e("i",{class:"fa-solid fa-spinner"})],-1),a=[t];function r(i,l){return _(),n("div",c,a)}const p=s(o,[["render",r]]);export{p as _};
-//# sourceMappingURL=Loader-4045377d.js.map
--- assets/Registration-c5e3b22e.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o as t,c}from"./main-ea7617f2.js";const o={};function n(r,a){return t(),c("div",null,"Your Acoount must be activated")}const _=e(o,[["render",n]]);export{_ as default};
-//# sourceMappingURL=Registration-c5e3b22e.js.map
